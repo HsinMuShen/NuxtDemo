c7bafeb26ef9bf3f38e2f0158603b461695ac8b5
沈昕牧 <michael_shen@chenxinmudeMacBook-Pro.local>
2023-01-24 00:05:29 +0800

    add posts page

---
+++ pages/posts/_id/index.vue
@@ -0,0 +1,12 @@
+<template lang="pug">
+.single-post-page
+    section.post    
+        h1 title
+</template>
+
+<style lang="stylus" scoped>
+  .post-pages
+    display flex
+    justify-content center
+    align-items center
+</style>
---
+++ pages/posts/index.vue
@@ -0,0 +1,10 @@
+<template lang="pug">
+.post-pages
+</template>
+
+<style lang="stylus" scoped>
+  .post-pages
+    display flex
+    justify-content center
+    align-items center
+</style>
