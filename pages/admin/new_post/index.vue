c5ca3d176eb2ca70d8b0bdcfb32a3d620f45dc52
michael_shen <electrycity84@gmail.com>
2023-03-19 16:55:18 +0800

    refactor: testing if the chatgpt get it2

--- pages/admin/new_post/index.vue
+++ pages/admin/new_post/index.vue
@@ -14,7 +14,7 @@
     methods: {
       onSubmitted(postData) {
         this.$store.dispatch('addPost', postData).then(() => {
-          console.log('test if the code review get it', postData)
+          console.log('test if the code review get it2', postData)
           this.$router.push('/admin')
         })
       },
