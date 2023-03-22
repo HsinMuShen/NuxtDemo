a78be4739b552b025528d4706755bb076ffc8cb6
michael_shen <electrycity84@gmail.com>
2023-03-22 13:22:01 +0800

    test edit

--- pages/admin/new_post/index.vue
+++ pages/admin/new_post/index.vue
@@ -14,7 +14,7 @@
     methods: {
       onSubmitted(postData) {
         this.$store.dispatch('addPost', postData).then(() => {
-          console.log(1111111)
+          console.log(111111)
           this.$router.push('/admin')
         })
       },
