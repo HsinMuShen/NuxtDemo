49ce64e7e1e7e7fd8853114b53db059a96973ee4
michael_shen <electrycity84@gmail.com>
2023-03-22 14:22:19 +0800

    test edit

--- store/index.js
+++ store/index.js
@@ -62,7 +62,7 @@ const createStore = () => {
     },
     getters: {
       loadedPosts(state) {
-        console.log(111111)
+        console.log(1111111)
         return state.loadedPosts
       },
     },
