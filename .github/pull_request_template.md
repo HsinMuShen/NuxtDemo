ce401f7b4b1de61b1638256679072584949fdb0b
michael_shen <electrycity84@gmail.com>
2023-04-05 20:14:13 +0800

    refactor(.github): delete unuse workflow

--- .github/pull_request_template.md
+++
@@ -1,9 +0,0 @@
-# Pull Request Template
-
-This template is used to create pull requests that list all modified file paths!
-
-$(./workflows/generate_file_list.sh)
-
-## Description
-
-Describe your changes here.
--- .github/workflows/generate_file_list.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-# Get the list of modified files
-MODIFIED_FILES=$(git diff --name-only)
-
-# Print the list of modified files
-echo "## Modified Files"
-echo "The following files have been modified in this pull request:"
-echo "$MODIFIED_FILES" | sed 's/^/- /'
